0e45a393867b5c0165956f23597f078bc97dea57
AshC4m <lykinsergey@yandex.ru>
2022-10-19 03:02:09 +0300

    task_01-done
    Сделал task_01

--- task_01.py
+++ task_01.py
@@ -5,3 +5,18 @@ my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start M
 
 # Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
 # Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
+
+
+first_song = my_favorite_songs [:my_favorite_songs.find(',')]
+
+last_song = my_favorite_songs [my_favorite_songs.find('N'):]
+
+second_song = my_favorite_songs[my_favorite_songs.find('S'):
+my_favorite_songs.find(', A')]
+
+secondend_song = my_favorite_songs [my_favorite_songs.find('Star'):
+my_favorite_songs.find(', N')]
+
+
+print(first_song,'\n', last_song,'\n',  second_song,'\n', secondend_song)
+
